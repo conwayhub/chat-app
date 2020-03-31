c31a181fceb2b5f1cc0d85db41a3141af0402b9e
conwayhub <ofdermott@gmail.com>
2020-03-31 18:15:02 +0100

    user will be prompted for username, which will then be set in the url

--- frontEnd/fe-index.js
+++ frontEnd/fe-index.js
@@ -1,24 +1,35 @@
-const chatForm = document.getElementById('chat-form');
+const chatForm = document.getElementById("chat-form");
 const socket = io();
 
-const { username } = Qs.parse(location.search, { ignoreQueryPrefix: true });
-socket.emit('join', { username });
+let inputtedName = null;
 
-socket.on('message', message => {
+if (window.location.href.indexOf("?") == -1) {
+  inputtedName = prompt("Username:", "anonymous");
+}
+
+if (inputtedName != null) {
+  let newURL = location.href + `?username=${inputtedName}`;
+  location.assign(newURL);
+}
+
+const { username } = Qs.parse(location.search, { ignoreQueryPrefix: true });
+socket.emit("join", { username });
+
+socket.on("message", message => {
   outputMessage(message);
 });
 
-chatForm.addEventListener('submit', e => {
+chatForm.addEventListener("submit", e => {
   e.preventDefault();
 
   const msg = e.target.elements.msg.value;
 
-  socket.emit('chatMessage', msg);
+  socket.emit("chatMessage", msg);
 });
 
 function outputMessage(message) {
-  const div = document.createElement('div');
-  div.classList.add('message');
+  const div = document.createElement("div");
+  div.classList.add("message");
   div.innerHTML = `<p>${message.time}</p><p class=text><span>${message.username}:</span>${message.message}</p>`;
-  document.querySelector('.chat-messages-output').appendChild(div);
+  document.querySelector(".chat-messages-output").appendChild(div);
 }
